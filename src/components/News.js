b191c792b7e14d87723cf04b15aecacaf9507d66
krishan <114896529+krishan-1@users.noreply.github.com>
2022-12-19 11:07:53 +0530

    component item setup

--- src/components/News.js
+++ src/components/News.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  static propTypes = {}
+  static propTypes = {};
 
   render() {
     return (
-      <div>News
-
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container-md container-fluid my-3">
+        <h2 className="text-center">NewsApp - Top Headlines</h2>
+        <div className="row justify-content-around g-2">
+          <div className="col-md-4 col-sm-6">
+            <NewsItem title="myTitle" desc="myDesc"/>
+          </div>
+          <div className="col-md-4 col-sm-6">
+            <NewsItem title="myTitle" desc="myDesc"/>
+          </div>
+          <div className="col-md-4 col-sm-6">
+            <NewsItem title="myTitle" desc="myDesc"/>
+          </div>
+          <div className="col-md-4 col-sm-6">
+            <NewsItem title="myTitle" desc="myDesc"/>
+          </div>
+        </div>
+        1982f1087a7741e39dcd68ca30f43f11
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-
   render() {
+    let { title, desc, imgUrl } = this.props;
     return (
-      <div>This is a news items</div>
-    )
+      <div>
+        <div className="card">
+          <img src="{imgUrl}" className="card-img-top" alt="{title}" />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{desc}</p>
+            <a href="#" className="btn btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
